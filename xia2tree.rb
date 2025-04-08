47fc893dec599d592a18ad8843a8767f39384ea1
Vetle W. Ingeberg <vetlewi@users.noreply.github.com>
2025-04-08 11:47:38 +0200

    xia2tree 0.9.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- xia2tree.rb
+++ xia2tree.rb
@@ -1,9 +1,9 @@
 class Xia2tree < Formula
     desc "XIA2tree: Toolkit for analysis of raw data from experiments with XIA Pixie-16 DGFs"
     homepage "https://github.com/vetlewi/XIA2tree"
-    url "https://github.com/vetlewi/XIA2tree/archive/refs/tags/v0.9.2.tar.gz"
-    version "0.9.2"
-    sha256 "0f8ddee7a94de8bae0ac53f8e9e6f937c22465f0eea669b53d37babb8ef7abce"
+    url "https://github.com/vetlewi/XIA2tree/archive/refs/tags/v0.9.3.tar.gz"
+    version "0.9.3"
+    sha256 "18b13933abd196147a96e512661e8ff029ef77ef197bbe11b1d9e28da991bcfe"
 
     depends_on "cmake" => [:build, :test]
     depends_on "root"
