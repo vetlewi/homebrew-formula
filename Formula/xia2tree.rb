69758b1e117f93609562355960175c99567df5db
Vetle W. Ingeberg <5293376+vetlewi@users.noreply.github.com>
2025-04-15 10:59:34 +0000

    xia2tree: add 0.9.8 bottle.

--- Formula/xia2tree.rb
+++ Formula/xia2tree.rb
@@ -5,6 +5,11 @@ class Xia2tree < Formula
   # version "0.9.6"
   sha256 "f6cef166180491ef6bf96500d742b5d8009ae8a57a65674a9f60b1503bd8242c"
 
+  bottle do
+    root_url "https://ghcr.io/v2/vetlewi/formula"
+    sha256 cellar: :any, arm64_sequoia: "6f21b0eda6e5d1bd0188a9d3d750927666cc33a765f35b1cce625ebc4f2169a1"
+  end
+
   depends_on "cmake" => [:build, :test]
   depends_on "root"
 
