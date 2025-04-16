141b78764f4fa531ba7e800c4f5c2455efec8b21
Vetle W. Ingeberg <vetlewi@fys.uio.no>
2025-04-16 09:30:31 +0200

    Update talys.rb

--- Formula/talys.rb
+++ Formula/talys.rb
@@ -6,11 +6,6 @@ class Talys < Formula
   sha256 "1c878aee255ed4b957ef8da0f31d7892052f0daa68b8489b9caae418da18acd3"
   license "MIT"
 
-  bottle do
-    root_url "https://github.com/vetlewi/homebrew-formula/releases/download/v1.0"
-    sha256 arm64_sonoma: "6a7b93743e08aa20c46c823099598d47f0e0bce18b817eb2ea5575723da2614f"
-  end
-
   depends_on "cmake" => :build
   depends_on "gcc"
 
