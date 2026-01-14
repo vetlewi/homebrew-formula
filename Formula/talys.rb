76236e20635c136573752ae4cd3f1494cbafc30a
Vetle W. Ingeberg <5293376+vetlewi@users.noreply.github.com>
2026-01-14 16:39:21 +0000

    talys: add 2.2 bottle.

--- Formula/talys.rb
+++ Formula/talys.rb
@@ -5,6 +5,16 @@ class Talys < Formula
   sha256 "824adb072f9a2f7fc3cf5720533af9de0552e137d8ed7209916821a618dc44a8"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/vetlewi/formula"
+    sha256                               arm64_tahoe:   "eba103a125ec4214503d89022301ddbf7fd44dd845fe8adc2011c10c1a1cf46f"
+    sha256                               arm64_sequoia: "fc2c3734a09e59c7c68eae2f53eec8e5adfa4fd5207427a41764fcb041920316"
+    sha256                               arm64_sonoma:  "8bf2e384d011719228920d4ebd179bf64e3c3d3b40bfce8c0831473258808754"
+    sha256 cellar: :any,                 sequoia:       "5bdc06c32d75a8f35656c258d392f6a7bc6167013affe937eeec30f08a34bcea"
+    sha256                               arm64_linux:   "d07a964bf076851f6cc31d2507eed7e6ecfab274c34687863223ce02e212d341"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e23dbf1a89e578c74fa093ca192f02dcf949dc3eed19bfc6955d4e66b6d6bc14"
+  end
+
   depends_on "cmake" => :build
   depends_on "gcc"
 
